7e46c163ad0c1afd212b10362ce1ef93282e6100
liesbeth <liesbeth@blend.be>
2017-03-03 09:40:30 +0100

    cloning op nieuwe computer, aanpassingen aan newsletter subscriber plugin search optie

--- craft/config/db.php
+++ craft/config/db.php
@@ -10,10 +10,10 @@
 return array(
 
 	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => '127.0.0.1',
+	'server' => 'localhost',
 
 	// The name of the database to select.
-	'database' => 'blend_craft',
+	'database' => 'blend-craft',
 
 	// The database username to connect with.
 	'user' => 'root',
